a94a3b4a7870ae4d176b57f3e5dc49a1431fa362
Stuart-Swan <140192252+Stuart-Swan@users.noreply.github.com>
2024-10-07 08:44:54 -0700

    Add files via upload

--- matchlib_examples/include/stable_random.h
+++ matchlib_examples/include/stable_random.h
@@ -26,11 +26,32 @@ public:
     const char* env_rand_seed = std::getenv("NVHLS_RAND_SEED");
     if (env_rand_seed != NULL) seed = atoi(env_rand_seed);
 
-    generator.seed(seed + hasher(handle.name()));
+
+    generator.seed(seed + hasher(clean_name(handle.name())));
   }
 
   unsigned get() {
     CONNECTIONS_ASSERT_MSG(sc_get_current_process_handle() == handle, "stable_random called from two processes");
     return distribution(generator);
+  }
+
+  std::string clean_name(std::string str) {
+    // Questa messes up SC pathnames, we clean them up if needed here..
+    std::string substr("sc_main/");
+    size_t pos = str.find(substr);
+
+    if (pos != std::string::npos) {
+        str.erase(pos, substr.length());
+    }
+
+    for (size_t i = 0; i < str.length(); ++i) {
+        if (str[i] == '/') {
+            str[i] = '.'; 
+        }
+    }
+
+    // std::cout << "PROC: " << str << "\n";
+
+    return str;
   }
 };
