54232e1ebb9035be4224cef1d7223327a40dce91
Stuart-Swan <sswan314@gmail.com>
2025-04-20 05:10:54 -0700

    updates

--- build_systemc.sh
+++ build_systemc.sh
@@ -8,9 +8,10 @@ cd ..
 
 export CXX="g++ -std=c++11"
 cd $SYSTEMC_HOME
+find . -name 'configure.ac' -exec touch aclocal.m4 configure Makefile.in {} +
 rm -rf objdir
 mkdir objdir
 cd objdir
-../configure
+../configure --disable-maintainer-mode
 make
 make install
