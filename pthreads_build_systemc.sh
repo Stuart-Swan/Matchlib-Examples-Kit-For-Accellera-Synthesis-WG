3f5eb3a60b3652cf6027b49c475ee95a443dedf6
Stuart-Swan <sswan314@gmail.com>
2025-04-21 06:10:55 -0700

    fixes

--- pthreads_build_systemc.sh
+++ pthreads_build_systemc.sh
@@ -8,9 +8,10 @@ cd ..
 
 export CXX="g++ -std=c++11"
 cd $SYSTEMC_HOME
+find . -name 'configure.ac' -exec touch aclocal.m4 configure Makefile.in {} +
 rm -rf objdir
 mkdir objdir
 cd objdir
-../configure --enable-pthreads
+../configure --enable-pthreads --disable-maintainer-mode
 make
 make install
