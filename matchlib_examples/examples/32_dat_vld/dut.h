0c5ab516e8f16f4213e8798fbf49e2b354eaf556
Stuart-Swan <140192252+Stuart-Swan@users.noreply.github.com>
2025-12-11 15:16:33 -0800

    Add files via upload

--- matchlib_examples/examples/32_dat_vld/dut.h
+++ matchlib_examples/examples/32_dat_vld/dut.h
@@ -14,26 +14,20 @@ public:
   sc_in<bool>    SC_NAMED(clk);
   sc_in<bool>    SC_NAMED(rst_bar);
 
-  dat_vld_in<T>   SC_NAMED(in1);
-  dat_vld_out<T>  SC_NAMED(out1);
-
-  dat_vld_in_xact<T>  SC_NAMED(in1_xact);
-  dat_vld_out_xact<T> SC_NAMED(out1_xact);
+  dat_vld_in_xact<T>   SC_NAMED(in1);
+  dat_vld_out_xact<T>  SC_NAMED(out1);
 
   SC_CTOR(dut) {
     SC_THREAD(main);
     sensitive << clk.pos();
     async_reset_signal_is(rst_bar, false);
-
-    in1_xact.bind(in1);
-    out1_xact.bind(out1);
   }
 
 private:
 
   void main() {
-    out1_xact.chan.ResetWrite();
-    in1_xact.chan.ResetRead();
+    out1.chan.ResetWrite();
+    in1.chan.ResetRead();
     wait(); 
 // NOTE: stall_mode must be stall for this example. If it is flush then FSM may apply backpressure,
 // which would cause the protocol adaptor to operate incorrectly
@@ -41,11 +35,11 @@ private:
 #pragma hls_pipeline_init_interval 2
 #pragma pipeline_stall_mode stall
     while (1) {
-      uint32 x = in1_xact.chan.Pop();
+      uint32 x = in1.chan.Pop();
       uint32 y = 0;
       y += 3 * x;
       y += 7 * x * x;
-      out1_xact.chan.Push(y);
+      out1.chan.Push(y);
     }
   }
 };
